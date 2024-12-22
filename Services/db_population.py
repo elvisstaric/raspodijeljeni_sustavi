b93f2678d7efe56874f5ac8694447593f2078509
Elvis <estaric@unipu.hr>
2024-12-22 14:20:44 +0100

    Zavrsen servis za populaciju db

--- Services/db_population.py
+++ Services/db_population.py
@@ -4,7 +4,6 @@ from aiohttp import web
 from aiohttp.web import AppRunner
 import asyncio, aiohttp
 
-
 async def post_db_population(req):
     data=await req.json()
     conn_str=data.get("conn_str")
@@ -20,12 +19,23 @@ async def post_db_population(req):
             if rel.get("fk") is None:
                 new_relation = pd.read_csv(PATH, usecols=rel.get("atributes"), delimiter=',')
                 unique = new_relation.drop_duplicates()
-                """ values={atr:unique[atr] for atr in rel.get("atributes")}
-                df_values = pd.DataFrame({'name':values.keys(), 'value':values.values()})"""
                 id = list(range(1,len(unique)+1))
                 unique.insert(0, "id", id)
-                print(unique)
                 unique.to_sql(con=mydb, name=rel.get("name"), if_exists='append', index=False)
+            else:
+                fk_list= []
+                for i, row in df.iterrows():
+                    table=pd.read_sql(f"""SELECT * from {rel.get("fk")}""", conn)
+                    unique_key = df[rel.get("uk")].iloc[i]
+                    fk_list.append((table['id'].loc[table[rel.get("uk")[0]]==unique_key.iloc[0]]).iloc[0])
+
+                new_relation = pd.read_csv(PATH, usecols=rel.get("atributes"), delimiter=',')
+                unique = new_relation.drop_duplicates()
+                id = list(range(1,len(unique)+1))
+                unique.insert(0, "id", id)
+                unique.insert(1, f"{rel.get("fk")}_id", fk_list)
+                unique.to_sql(con=mydb, name=rel.get("name"), if_exists='append', index=False)
+
         conn.close()
         rez="Populacija baze uspjesna!"
     except Exception as e:
@@ -49,7 +59,8 @@ async def main():
         PATH = input("Unesite lokaciju CSV datoteke: ")
         relation_nr = int(input("Unesite broj relacija: "))
         relations = [{"name": input(f"Naziv relacije {i+1}: "), "atributes":input(f"Atributi relacije {i+1}: ").split(","),
-                "fk": input(f"Strani kljuc relacije {i+1}(ako nema nemojet unijeti nista) : " ) or None} for i in range(relation_nr)]
+                "fk": input(f"Relacija stranog kljuca relacije {i+1}(ako nema nemojet unijeti nista) : " ) or None,
+                "uk": input(f"Unique key u relaciji stranog kljuca {i+1}(ako nema nemojet unijeti nista) : " ).split(",")} for i in range(relation_nr)]
         data={"conn_str":conn_str, "path":PATH, "relations":relations}
 
         population_rez=await session.post("http://localhost:8080/db_population", json=data)
