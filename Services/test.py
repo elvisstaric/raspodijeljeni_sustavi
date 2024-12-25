a450ad5bd9d916ac44e1531b4a2603987c593063
Elvis <estaric@unipu.hr>
2024-12-25 18:07:08 +0100

    Update test - dodane sve metode

--- Services/test.py
+++ Services/test.py
@@ -18,14 +18,43 @@ async def get_routes():
     return await get_settings.json()
 
 async def run_test(test_select, url):
-    if(test_select.get("method")=="get"):
-        async with aiohttp.ClientSession() as session:
-            test_start=time.time()
-            res=await session.get(url+test_select.get("endpoint"))
-            test_end=time.time()
-            return (await res.json(), test_end-test_start)
-        
     
+        if test_select.get("method")=="get":
+            async with aiohttp.ClientSession() as session:
+                test_start=time.time()
+                res=await session.get(url+test_select.get("endpoint"))
+                test_end=time.time()
+                return (await res.json(), test_end-test_start)
+        elif test_select.get("method")=="post":
+            async with aiohttp.ClientSession() as session:
+                test_start=time.time()
+                res=await session.post(url+test_select.get("endpoint"), json=test_select.get("payload"))
+                test_end=time.time()
+                return (await res.json(), test_end-test_start)
+        elif test_select.get("method")=="put":
+            async with aiohttp.ClientSession() as session:
+                test_start=time.time()
+                res=await session.put(url+test_select.get("endpoint"), json=test_select.get("payload"))
+                test_end=time.time()
+                return (await res.json(), test_end-test_start)
+        elif test_select.get("method")=="patch":
+            async with aiohttp.ClientSession() as session:
+                test_start=time.time()
+                res=await session.patch(url+test_select.get("endpoint"), json=test_select.get("payload"))
+                test_end=time.time()
+                return (await res.json(), test_end-test_start)
+        elif test_select.get("method")=="delete":
+            async with aiohttp.ClientSession() as session:
+                test_start=time.time()
+                res=await session.delete(url+test_select.get("endpoint"))
+                test_end=time.time()
+                return (await res.json(), test_end-test_start)
+        else:
+            pass
+    
+    
+        
+
 
 def test(lat, cnt, run, url, tests):
         max_lat=0
